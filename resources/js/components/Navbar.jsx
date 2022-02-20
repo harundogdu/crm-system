3ac2571a8eac93ccdf7960809119c6f8294aa7f5
Harun Doğdu <harundogdu06@gmail.com>
2022-02-20 20:16:47 +0300

    navbar component created

---
+++ resources/js/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <NavLink className="navbar-brand" to={"/"}>HD CRM SYSTEM</NavLink>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"/>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav m-auto me-2">
+                        <li className="nav-item">
+                            <NavLink className="nav-link active" aria-current="page" to="/products">Products</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link active" aria-current="page"
+                                     to="/categories">Categories</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link active" aria-current="page" to="/accounts">Accounts</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link active" aria-current="page" to="/stocks">Stocks</NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
